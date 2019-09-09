a451bba418254dfe5bd24fa4f0cc1f87c4a70955
tigermeal <tigermeal0125@gmail.com>
2019-09-09 18:06:19 +0900

    :white_check_mark: Update codes about Class methods & Instance methods to Dog.java and DogLauncher.java

--- java/intro/1-2/Dog.java
+++ java/intro/1-2/Dog.java
@@ -1,5 +1,6 @@
 public class Dog {
 	public int weightInPounds;
+	public static String familyName = "Tigermeal family";
 
 	public Dog(int w) {
 		weightInPounds = w;
@@ -13,6 +14,22 @@ public class Dog {
 		} else  {
 			System.out.println("woooof!");
 		}
+	}
+
+// Class Methods, static methods 
+	public static Dog maxDog(Dog d1, Dog d2) {
+		if (d1.weightInPounds > d2.weightInPounds) {
+			return d1;
+		}
+		return d2;
+	}
+
+// Instance Methods, non-static methods
+	public Dog maxDog(Dog d2) {
+		if (this.weightInPounds > d2.weightInPounds) {
+			return this;
+		}
+		return d2;
 	}
 }
 
