b78e7acf192e312d913a1e450ddddce2bc0df7cf
tigermeal <tigermeal0125@gmail.com>
2019-09-10 19:23:59 +0900

    :white_check_mark: Update codes about Command Line Arguments

---
+++ java/intro/1-2/ArgsDemo.java
@@ -0,0 +1,8 @@
+public class ArgsDemo {
+	public static void main(String[] args) {
+		System.out.println(args[0]);
+		System.out.println("----");
+		System.out.println(args[1]);
+		System.out.println("----");
+	}
+}
