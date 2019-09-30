e102eba45dbe8e681a3b9231b8690a820f9443b7
tigermeal <tigermeal0125@gmail.com>
2019-09-30 22:25:46 +0900

    :sparkles: Parameter Passing

--- java/lists/2-1/PollQuestions.java
+++ java/lists/2-1/PollQuestions.java
@@ -1,19 +1,34 @@
 public class PollQuestions {
     public static void main(String[] args) {
-        Walrus a = new Walrus(1000, 8.3);
-        Walrus b;
-        b = a;
-        b.weight = 5;
-        System.out.println(a);
-        System.out.println(b);
+        Walrus walrus = new Walrus(3500, 10.5);
+        int x = 9;
 
-        int x = 5;
-        int y;
-        y = x;
-        x = 2;
-        System.out.println("x is: " + x);
-        System.out.println("y is: " + y);
+        doStuff(walrus, x);
+        System.out.println(walrus.weight);
+        System.out.println(x);
     }
+
+    public static void doStuff(Walrus W, int x) {
+        W.weight = W.weight - 100;
+        x = x - 5;
+        // x in doStuff does change, but in main doesn't change
+    }
+
+    // public static void main(String[] args) {
+    //     Walrus a = new Walrus(1000, 8.3);
+    //     Walrus b;
+    //     b = a;
+    //     b.weight = 5;
+    //     System.out.println(a);
+    //     System.out.println(b);
+
+    //     int x = 5;
+    //     int y;
+    //     y = x;
+    //     x = 2;
+    //     System.out.println("x is: " + x);
+    //     System.out.println("y is: " + y);
+    // }
 
     public static class Walrus {
         public int weight;
@@ -23,9 +38,9 @@ public class PollQuestions {
             weight = w;
             tuskSize = ts;
         }
-
-        public String toString() {
-            return String.format("weight: %d, tusk size: %.2f", weight, tuskSize);
-        }
     }
+    //     public String toString() {
+    //         return String.format("weight: %d, tusk size: %.2f", weight, tuskSize);
+    //     }
+    // }
 }
