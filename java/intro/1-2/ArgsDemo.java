a0f687923742868a9ae350e8fd0fcd1f342fb4c0
tigermeal <tigermeal0125@gmail.com>
2019-12-09 19:17:41 +0900

    :fire: Clean up java files

--- java/intro/1-2/ArgsDemo.java
+++
@@ -1,8 +0,0 @@
-public class ArgsDemo {
-	public static void main(String[] args) {
-		System.out.println(args[0]);
-		System.out.println("----");
-		System.out.println(args[1]);
-		System.out.println("----");
-	}
-}
